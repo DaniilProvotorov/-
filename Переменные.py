360473738bb553f785a6ef755d75b16970cc93f6
DaniilProvotorov <dantcsar@gmail.com>
2024-10-03 10:16:04 +0500

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,10 @@
+Noct = 12 #кол-во выполненных заданий Number_of_completed_tasks
+Nohs = 1.5 #кол-во затраченных часов Number_of_hours_spent
+Course = 'Python'
+Time_for_one_task = Nohs/Noct
+print('Курс:', Course)
+print('Количество выполненных заданий:', Noct)
+print('Количество затраченных часов:', Nohs)
+print('Среднеевремя выполнения:', Time_for_one_task)
+
+print('Курс:', Course,'Количество выполненных заданий:', Noct,'Количество затраченных часов:', Nohs,'Среднеевремя выполнения:', Time_for_one_task) #можно одной строкой все перечислить
